f5dbed207d50c2c6aa72a7b2bc3ba3fec310da91
Axelby <ads_brest@mail.tu>
2014-11-18 13:30:02 -0800

    Second commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 class Helloworld
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Privet Aleksey!");
-	}
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String n;
+        System.out.println("Inpute your parameter:");
+        n = in.next();
+        System.out.println("Privet, " + n + "!!!");
+        }
 }
