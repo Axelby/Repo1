39e54ac2a35b0a9f2dcdb719f194a8b29342c95f
Axelby <ads_brest@mail.tu>
2014-11-22 09:41:07 -0800

    Main Paramater

--- Helloworld.java
+++ Helloworld.java
@@ -1,12 +1,7 @@
-import java.util.Scanner;
 class Helloworld
 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        String n;
-        System.out.println("Inpute your parameter:");
-        n = in.next();
-        System.out.println("Privet, " + n + "!!!");
-        }
+        System.out.println("Privet, " + args[0] + "!!!");
+    }
 }
